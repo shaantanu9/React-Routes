0b628ba5c153f7b5430b8357f529768818a650f5
Shantanu Bombatkar <33876955+shaantanu9@users.noreply.github.com>
2022-03-20 15:17:39 +0530

    Added the Basic route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import "./styles.css";
@@ -5,8 +6,11 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
+      <Routes>
+        <Route path="/" element={<About />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
