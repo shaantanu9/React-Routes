27546bcc4309c18373588ebea131d8cc531b368c
Shantanu Bombatkar <33876955+shaantanu9@users.noreply.github.com>
2022-03-20 14:54:46 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ const About = (props) => {
   return (
     <>
       <h1 className="animate-bounce bg-indigo-700">
-        Checking About Component Working{" "}
+        Checking About Component Working Checking About Component Working{" "}
       </h1>
     </>
   );
